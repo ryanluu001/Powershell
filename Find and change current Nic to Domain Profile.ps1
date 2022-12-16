46e9f018f0766b654ece75e410dec4da08423bf3
ryanluu001 <87357410+ryanluu001@users.noreply.github.com>
2022-12-16 11:18:30 +0800

    Add files via upload

---
+++ Find and change current Nic to Domain Profile.ps1
@@ -0,0 +1,18 @@
+﻿#Get the current network adapter profile name
+$interface = Get-NetConnectionProfile 
+$interfaceAlias = $interface.InterfaceAlias
+
+# Find and set current network profile to Domain
+Set-Location -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles"
+$keys = Get-ChildItem
+foreach($key in $keys)
+{
+$profilename = Get-ItemPropertyValue -Path $key.PSPath -Name Profilename
+if($profilename = $interfaceAlias)
+{
+Write-Host "Changing key for network profile" $interfaceAlias
+Set-ItemProperty -Path $key.PSPath -Name 'Category' -Value '2'
+Stop-Service NlaSvc -Force -Verbose
+Start-Service NlaSvc -Verbose
+}
+}
